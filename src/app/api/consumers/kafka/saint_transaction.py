82928b4c1ac4e01a5c0229d8d4e23a3fedce3b22
Jay Faust <jay.faust3@gmail.com>
2023-07-29 00:55:23 -0400

    WIP: Added handling in consumer

--- src/app/api/consumers/kafka/saint_transaction.py
+++ src/app/api/consumers/kafka/saint_transaction.py
@@ -1,4 +1,5 @@
 from typing import List
+from os import getenv
 from app.api.consumers.kafka.base import BaseConsumer
 from app.data_access.data_stores.sql.query_handlers.saint_transaction import SaintTransactionQueryHandler
 from app.core.data_models.saint import Saint
@@ -16,6 +17,9 @@ class SaintTransactionConsumer(BaseConsumer):
             broker_group_id,
             topics
         )
+        self.__saint_creation_topic: str = getenv('SAINT_CREATION_KAFKA_TOPIC')
+        self.__saint_update_topic: str = getenv('SAINT_UPDATE_KAFKA_TOPIC')
+        self.__saint_deletion_topic: str = getenv('SAINT_DELETION_KAFKA_TOPIC')
         self.__saint_transaction_query_handler: SaintTransactionQueryHandler = saint_transaction_query_handler
 
     def consume(self) -> None:
@@ -29,5 +33,44 @@ class SaintTransactionConsumer(BaseConsumer):
                 print("Consumer error happened: {}".format(msg.error()))
                 continue
 
-            print("Connected to Topic: {} and Partition : {}".format(msg.topic(), msg.partition()))
-            print("Received Message : {} with Offset : {}".format(msg.value().decode('utf-8'), msg.offset()))
+            topic = msg.topic()
+            message_data = msg.value().decode('utf-8')
+            print('Connected to Topic: {} and Partition : {}'.format(topic, msg.partition()))
+            print('Received Message : {} with Offset : {}'.format(message_data, msg.offset()))
+
+            match topic:
+                case self.__saint_creation_topic:
+                     saint = Saint(message_data._id(),
+                                   message_data.createdDate(),
+                                   message_data.modifiedDate(),
+                                   message_data.name(),
+                                   message_data.yearOfBirth(),
+                                   message_data.yearOfDeath(),
+                                   message_data.region(),
+                                   message_data.martyred(),
+                                   message_data.notes(),
+                                   message_data.hasAvatar())
+
+                     self.__saint_transaction_query_handler.handle_create_and_update(saint)
+
+                case self.__saint_update_topic:
+                     saint = Saint(message_data._id(),
+                                   message_data.createdDate(),
+                                   message_data.modifiedDate(),
+                                   message_data.name(),
+                                   message_data.yearOfBirth(),
+                                   message_data.yearOfDeath(),
+                                   message_data.region(),
+                                   message_data.martyred(),
+                                   message_data.notes(),
+                                   message_data.hasAvatar())
+
+                     self.__saint_transaction_query_handler.handle_create_and_update(saint)
+
+                case self.__saint_transaction_query_handler:
+                     saint_id: str = message_data.id()
+
+                     self.__saint_transaction_query_handler.handle_delete(saint_id)
+
+                case _:
+                    raise Exception('{} is not a supported topic'.format())
